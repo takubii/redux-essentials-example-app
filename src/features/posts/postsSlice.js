217e480b1a6afff9115b7f794c9ec8ab5ccf2622
takubii <vicsat93@gmail.com>
2023-09-05 14:23:23 +0900

    [add]投稿時刻表示を追加

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,8 +1,21 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit'
+import { sub } from 'date-fns'
 
 const initialState = [
-  { id: '1', title: 'First Post!', content: 'Hello!', user: '0' },
-  { id: '2', title: 'Second Post', content: 'More text', user: '1' },
+  {
+    id: '1',
+    title: 'First Post!',
+    content: 'Hello!',
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+    user: '0',
+  },
+  {
+    id: '2',
+    title: 'Second Post',
+    content: 'More text',
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+    user: '1',
+  },
 ]
 
 const postsSlice = createSlice({
@@ -17,6 +30,7 @@ const postsSlice = createSlice({
         return {
           payload: {
             id: nanoid(),
+            date: new Date().toISOString(),
             title,
             content,
             user: userId,
