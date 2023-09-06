e64b12073880b04c471b0e88da618c3134b1bf33
takubii <vicsat93@gmail.com>
2023-09-06 15:29:17 +0900

    [improve]selector関数をsliceフォルダに移動

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -5,9 +5,10 @@ import { Link } from 'react-router-dom'
 import { PostAuthor } from './PostAuthor'
 import { ReactionButtons } from './ReactionButtons'
 import { TimeAgo } from './TimeAgo'
+import { selectAllPosts } from './postsSlice'
 
 export const PostsList = () => {
-  const posts = useSelector((state) => state.posts)
+  const posts = useSelector(selectAllPosts)
 
   const orderedPosts = posts
     .slice()
