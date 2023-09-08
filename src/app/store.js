4d54406a881ebd921e29750383b5e8bcaca46d03
takubii <vicsat93@gmail.com>
2023-09-08 14:14:45 +0900

    [add]通知用のSliceを作成

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+import notificationsReducer from '../features/notifications/notificationsSlice'
 import postsReducer from '../features/posts/postsSlice'
 import usersReducer from '../features/users/usersSlice'
 
@@ -7,5 +8,6 @@ export default configureStore({
   reducer: {
     posts: postsReducer,
     users: usersReducer,
+    notifications: notificationsReducer,
   },
 })
