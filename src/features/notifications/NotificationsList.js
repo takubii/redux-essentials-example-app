716f83002dec837abbf89b4d5ebdb158aa245236
takubii <vicsat93@gmail.com>
2023-09-11 11:57:36 +0900

    [add]未読通知処理を追加

--- src/features/notifications/NotificationsList.js
+++ src/features/notifications/NotificationsList.js
@@ -1,14 +1,23 @@
+import classnames from 'classnames'
 import { formatDistanceToNow, parseISO } from 'date-fns'
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useLayoutEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 import { selectAllUsers } from '../users/usersSlice'
 
-import { selectAllNotifications } from './notificationsSlice'
+import {
+  allNotificationsRead,
+  selectAllNotifications,
+} from './notificationsSlice'
 
 export const NotificationsList = () => {
+  const dispatch = useDispatch()
   const notifications = useSelector(selectAllNotifications)
   const users = useSelector(selectAllUsers)
+
+  useLayoutEffect(() => {
+    dispatch(allNotificationsRead())
+  })
 
   const renderedNotifications = notifications.map((notification) => {
     const date = parseISO(notification.date)
@@ -17,8 +26,12 @@ export const NotificationsList = () => {
       name: 'Unknown User',
     }
 
+    const notificationClassname = classnames('notification', {
+      new: notification.isNew,
+    })
+
     return (
-      <div key={notification.id} className="notification">
+      <div key={notification.id} className={notificationClassname}>
         <div>
           <b>{user.name}</b> {notification.message}
         </div>
